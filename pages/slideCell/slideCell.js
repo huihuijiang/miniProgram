f6a76fb3537222dd4dac9bc8a9799a77862cd245
赵加会 <863697033@qq.com>
2019-05-16 14:44:14 +0800

    Update slideCell.js

--- pages/slideCell/slideCell.js
+++ pages/slideCell/slideCell.js
@@ -9,11 +9,34 @@ Page({
         hasUserInfo: false,
         canIUse: wx.canIUse('button.open-type.getUserInfo'),
         list: [
-            { text: '微信好友1', left: 0, id: 0 },
-            { text: '微信好友2', left: 0, id: 1 },
-            { text: '微信好友3', left: 0, id: 2 }
-        ]
+            { text: '微信好友1', left: 0, id: 10 },
+            { text: '微信好友2', left: 0, id: 11 },
+            { text: '微信好友3', left: 0, id: 12 }
+        ],
+		right:[
+			{
+				text: '哈哈',
+				background: 'red',
+				color: 'white',
+				width: 80,
+				handle:(event)=>{
+					console.log(event)
+				}
+			},
+			{
+				text:'删除',
+				background:'gray',
+				color:'black',
+				width:50,
+				handle: (event) => {
+					console.log(event)
+				}
+			}
+		]
     },
+	refresh:function(){
+		console.log(this.selectComponent('slide-cell'))
+	},
     onMyEvent: function (e) {//右侧滑块的点击事件
 
         var id = e.detail.id;
@@ -47,8 +70,14 @@ Page({
     },
 
     onLoad: function () {
-
+		console.log("onload")
     },
+	onReady: function () {
+		console.log("onready")
+	},
+	onShow: function () {
+		console.log("onshow")
+	},
 
 })
 

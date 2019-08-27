b0436aa0778d6d76403349af3a1e29138d74420f
huihuijiang <863697033@qq.com>
2019-08-27 10:39:30 +0800

    可拖拽浮标
    添加动画

--- component/drafting-block/draft-block.js
+++ component/drafting-block/draft-block.js
@@ -15,7 +15,7 @@ var
 Component({
    
     properties: {
-
+width:String
     },
 
     
@@ -28,6 +28,7 @@ Component({
         query.select('#box').boundingClientRect()
         query.selectViewport().scrollOffset()
         query.exec(function (res) {
+          console.log(res)
             boxW = res[0].width;
             boxH = res[0].height;
         })
@@ -39,23 +40,49 @@ Component({
             y0      = e.currentTarget.offsetTop;
             pageX0  = e.touches[0].pageX;
             pageY0  = e.touches[0].pageY;
-            console.log(e)
+            console.log(e,'start')
+          this.setData({
             
+            style: ''
+          })          
         },
         move: function (e) {
             pageX   = e.changedTouches[0].pageX;
             pageY   = e.changedTouches[0].pageY;
             var _x  = pageX - pageX0;
             var _y  = pageY - pageY0;
+            console.log(_y)
             var _left  = (x0 + _x) < 0 ? 0 : (x0 + _x) > (w - boxW) ? (w - boxW) : (x0 + _x);
             var _top   = (y0 + _y) < 0 ? 0 : (y0 + _y) > (h - boxH) ? (h - boxH) : (y0 + _y);
+          console.log(y0 + _y)
             this.setData({
                 left : _left,
-                top  : _top
+                top  : _top,
+              // style: 'transform: translate(' + _left + 'px, '+_top+'px);transition:all 0.3s linear 0.01s'
             })
+            console.log('move')
             
         },
         end: function (e) {
+          // 动画
+          var _this = this;
+          setTimeout(function(){
+            console.log(_this.data.left)
+            if (_this.data.left + 50 > w / 2) {
+              // 靠右
+              _this.setData({
+                left: w - 100,
+                style: 'transform: translate(' +0 + 'px, 0);transition:all 0.3s linear 0.01s;'
+              })
+            } else {
+              // 靠左
+              _this.setData({
+                left: 0,
+                style: 'transform: translate(' + 0 + 'px, 0);transition:all 0.3s linear 0.01s;'
+              })
+            }
+          },100)
+          
             
         },
     }
